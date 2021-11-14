e96da2fbd3e3d013f9e3ba165c1779a6d0b162eb
Aros_zi34@student.itstep.org <Aros_zi34@student.itstep.org>
2021-11-14 22:36:08 +0200

    Добавьте файлы проекта.

---
+++ Drunkard/Karta.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drunkard
+{
+    //♣️ Трефы — clubs
+    //♦️ Бубны — diamonds
+    //♥️ Червы — hearts
+    //♠️ Пики — spades
+    enum NameKart
+    {
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King,
+        Ace
+    }
+
+    enum SuitKart
+    {
+        Clubs,
+        Diamonds,
+        Hearts,
+        Spades
+    }
+
+    class Karta
+    {
+        private string [,]AllSuit = new string[9 , 4];
+
+        //Индексатор
+        public string this[int col, int row]
+        {
+            get { return AllSuit[col,row]; }
+            set { AllSuit[col, row] = value; }
+        }
+
+        public string Name { get; set; }
+        public string Suit { get; set; }
+
+        public void FillCard()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    //NameKart Name = (NameKart)Enum.GetValues(typeof(NameKart)).GetValue(i);
+                   // SuitKart Suit = (SuitKart)Enum.GetValues(typeof(SuitKart)).GetValue(j);
+
+                    AllSuit[i, j] = (NameKart)Enum.GetValues(typeof(NameKart)).GetValue(i) + " " + (SuitKart)Enum.GetValues(typeof(SuitKart)).GetValue(j);
+
+                    if(j == 0)
+                        AllSuit[i, j] += " (трефа)";
+                    if (j == 1)
+                        AllSuit[i, j] += " (бубна)";
+                    if (j == 2)
+                        AllSuit[i, j] += " (чирва)";
+                    if (j == 3)
+                        AllSuit[i, j] += " (пика)";
+
+                }
+
+            }
+        }
+
+        public void PrintAllSuit()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {                    
+                    Console.WriteLine(AllSuit[i, j]);          
+                }
+            }
+        }
+
+     
+
+    }
+}
---
+++ Drunkard/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drunkard
+{
+    class Program
+    {
+        public static void Shuffle<T>(T[] arr)
+        {
+            Random rand = new Random();
+
+            for (int i = arr.Length - 1; i >= 1; i--)
+            {
+                int j = rand.Next(i + 1);
+
+                T tmp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = tmp;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Karta karta = new Karta();
+
+            Random random = new Random();
+
+            karta.FillCard();
+            // karta.PrintAllSuit();
+
+            string[] arr = new string [36];
+            int count = 0;
+
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    arr[count] = karta[i, j];
+                    count++;
+                }
+            }
+
+            //foreach (var item in arr)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            Shuffle(arr);
+
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+
+            //try
+            //{
+            //    Console.WriteLine(karta[10, 67]);
+            //}
+            //catch (Exception ex)
+            //{
+
+            //    Console.WriteLine(ex);
+            //}
+
+
+
+
+            //for (int i = 0; i < 9; i++)
+            //{
+            //    for (int j = 0; j < 4; j++)
+            //    {
+            //        Console.WriteLine(karta[i, j]);
+
+            //    }
+
+            //}
+        }
+    }
+}
